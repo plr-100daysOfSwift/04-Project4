bd3961cbf6b965506b66f94bd318b725f470d1a2
Paul Richardson <developer@photographerafoot.com>
2021-04-07 08:24:59 +1000

    CHG: override loadView; set navigation delegate to self; allow navigation gestures

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
 	var webView: WKWebView!
 
+	override func loadView() {
+		webView = WKWebView()
+		webView.navigationDelegate = self
+		webView.allowsBackForwardNavigationGestures = true
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
